a5e8debc57e02356a2923a9ab35982268a4bee3b
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-02-27 16:45:23 -0500

    feat(core): initialize Spring Boot application with Maven wrapper and configuration files

---
+++ src/test/java/gov/cancer/ccr/oit/pres/sonardemo/SonarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cancer.ccr.oit.pres.sonardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
